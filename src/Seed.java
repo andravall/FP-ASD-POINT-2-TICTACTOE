c8f24ffd4fa08f885bed98fd679e113c4ddfe47c
Fachreza28 <162418737+Fachreza28@users.noreply.github.com>
2024-12-17 16:52:19 +0700

    Update Seed.java

--- src/Seed.java
+++ src/Seed.java
@@ -1,22 +1,20 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231159 - Mohammad Ferdinand Valliandra
+ * 2 - 5026231135 - Fachreza Aptadhi Kurniawan
+ * 3 - 5026231149 - Ananda Donelly Reksana
+ */
+
 import java.awt.Image;
 import java.net.URL;
 import javax.swing.ImageIcon;
-/**
- * This enum is used by:
- * 1. Player: takes value of CROSS or NOUGHT
- * 2. Cell content: takes value of CROSS, NOUGHT, or NO_SEED.
- *
- * We also attach a display image icon (text or image) for the items.
- *   and define the related variable/constructor/getter.
- * To draw the image:
- *   g.drawImage(content.getImage(), x, y, width, height, null);
- *
- * Ideally, we should define two enums with inheritance, which is,
- *  however, not supported.
- */
+
 public enum Seed {   // to save as "Seed.java"
-    CROSS("X", "Mogh.gif"),   // displayName, imageFilename
-    NOUGHT("O", "ranni.gif"),
+    CROSS("X", "Image/Human.png"),   // displayName, imageFilename
+    NOUGHT("O", "Image/AI.png"),
     NO_SEED(" ", null);
 
     // Private variables
