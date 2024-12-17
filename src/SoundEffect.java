aa8948ce615b37d654a283358f64d1f558d30236
Fachreza28 <162418737+Fachreza28@users.noreply.github.com>
2024-12-17 16:52:37 +0700

    Update SoundEffect.java

--- src/SoundEffect.java
+++ src/SoundEffect.java
@@ -1,3 +1,12 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231159 - Mohammad Ferdinand Valliandra
+ * 2 - 5026231135 - Fachreza Aptadhi Kurniawan
+ * 3 - 5026231149 - Ananda Donelly Reksana
+ */
 
 import java.io.IOException;
 import java.net.URL;
@@ -6,22 +15,12 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
-/**
- * This enum encapsulates all the sound effects of a game, so as to separate the sound playing
- * codes from the game codes.
- * 1. Define all your sound effect names and the associated wave file.
- * 2. To play a specific sound, simply invoke SoundEffect.SOUND_NAME.play().
- * 3. You might optionally invoke the static method SoundEffect.initGame() to pre-load all the
- *    sound files, so that the play is not paused while loading the file for the first time.
- * 4. You can the static variable SoundEffect.volume to SoundEffect.Volume.MUTE
- *    to mute the sound.
- *
- * For Eclipse, place the audio file under "src", which will be copied into "bin".
- */
+
 public enum SoundEffect {
-    EAT_FOOD("eatfood.wav"),
-    EXPLODE("explode.wav"),
-    DIE("die.wav");
+    EAT_FOOD("Audio/eatfood.wav"),
+    EXPLODE("Audio/explode.wav"),
+    DIE("Audio/die.wav"),
+    WIN("Audio/win.wav");
 
     /** Nested enumeration for specifying volume */
     public static enum Volume {
@@ -38,17 +37,16 @@ public enum SoundEffect {
         try {
             // Use URL (instead of File) to read from disk and JAR.
             URL url = this.getClass().getClassLoader().getResource(soundFileName);
+            if (url == null) {
+                throw new IOException("Sound file not found: " + soundFileName);
+            }
             // Set up an audio input stream piped from the sound file.
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(url);
             // Get a clip resource.
             clip = AudioSystem.getClip();
             // Open audio clip and load samples from the audio input stream.
             clip.open(audioInputStream);
-        } catch (UnsupportedAudioFileException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (LineUnavailableException e) {
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
     }
@@ -56,15 +54,16 @@ public enum SoundEffect {
     /** Play or Re-play the sound effect from the beginning, by rewinding. */
     public void play() {
         if (volume != Volume.MUTE) {
-            if (clip.isRunning())
-                clip.stop();   // Stop the player if it is still running
-            clip.setFramePosition(0); // rewind to the beginning
-            clip.start();     // Start playing
+            if (clip.isRunning()) {
+                clip.stop(); // Stop the player if it is still running
+            }
+            clip.setFramePosition(0); // Rewind to the beginning
+            clip.start(); // Start playing
         }
     }
 
-    /** Optional static method to pre-load all the sound files. */
+    /** Optional static method to preload all the sound files. */
     static void initGame() {
-        values(); // calls the constructor for all the elements
+        values(); // Calls the constructor for all the elements
     }
 }
