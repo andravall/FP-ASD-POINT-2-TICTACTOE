1ff65e5fc58cf792979843f46a6cb63f7164c959
Mohammad Ferdinand Valliandra <80774713+andravall@users.noreply.github.com>
2024-12-17 16:53:04 +0700

    Update State.java

--- src/State.java
+++ src/State.java
@@ -1,6 +1,14 @@
 /**
- *  The enum State contains the various game states of the TTT game
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #1
+ * 1 - 5026231159 - Mohammad Ferdinand Valliandra
+ * 2 - 5026231135 - Fachreza Aptadhi Kurniawan
+ * 3 - 5026231149 - Ananda Donelly Reksana
  */
+
+
 public enum State {  // to save as "State.java"
     PLAYING, DRAW, CROSS_WON, NOUGHT_WON
 }
